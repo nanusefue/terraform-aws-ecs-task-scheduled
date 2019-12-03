6d374deeea5fd608261be099ebb03445f1f327b7
Alex Vaque <alex.vaque@letgo.com>
2019-12-03 11:39:50 +0100

    enabling task_execution

--- main.tf
+++ main.tf
@@ -6,10 +6,10 @@ data "aws_iam_role" "ec2Role" {
   name = "${var.name_iam_role}"
 }
 
-data "aws_ecs_task_definition" "service" {
-  count = "${length(var.crontabs)}"
-  task_definition = "${var.crontabs[count.index].task_definition}"
-}
+#data "aws_ecs_task_definition" "service" {
+#  count = "${length(var.crontabs)}"
+#  task_definition = "${var.crontabs[count.index].task_definition}"
+#}
 
 resource "aws_cloudwatch_event_rule" "scheduled_task" {
   count = "${length(var.crontabs)}"
@@ -26,7 +26,8 @@ resource "aws_cloudwatch_event_target" "scheduled_task" {
   role_arn  = "${data.aws_iam_role.ec2Role.arn}"
   ecs_target {
     task_count          = "1"
-    task_definition_arn = "${data.aws_ecs_task_definition.service[count.index].id}"
+    #XEIC#task_definition_arn = "${data.aws_ecs_task_definition.service[count.index].id}"
+    task_definition_arn = aws_ecs_task_definition.default[0].arn
   }
 
 input = <<DOC
@@ -40,3 +41,49 @@ input = <<DOC
 }
 DOC
 }
+
+
+
+# ECS Task Definitions # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definitions.html # https://www.terraform.io/docs/providers/aws/r/ecs_task_definition.html
+resource "aws_ecs_task_definition" "default" {
+  count = "${var.enabled == true ? 1 : 0}"
+
+  # A unique name for your task definition.
+  family = var.service
+
+  # The ARN of the task execution role that the Amazon ECS container agent and the Docker daemon can assume.
+  execution_role_arn = var.create_ecs_task_execution_role ? join("", aws_iam_role.ecs_task_execution.*.arn) : var.ecs_task_execution_role_arn
+
+  # A list of container definitions in JSON format that describe the different containers that make up your task.
+  # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html#container_definitions
+  container_definitions = var.container_definitions
+
+  # The number of CPU units used by the task.
+  # It can be expressed as an integer using CPU units, for example 1024, or as a string using vCPUs, for example 1 vCPU or 1 vcpu.
+  # String values are converted to an integer indicating the CPU units when the task definition is registered.
+  # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html#task_size
+  cpu = var.cpu
+
+  # The amount of memory (in MiB) used by the task.
+  # It can be expressed as an integer using MiB, for example 1024, or as a string using GB, for example 1GB or 1 GB.
+  # String values are converted to an integer indicating the MiB when the task definition is registered.
+  # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html#task_size
+  memory = var.memory
+
+  # The launch type that the task is using.
+  # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html#requires_compatibilities
+  requires_compatibilities = var.requires_compatibilities
+
+  # Fargate infrastructure support the awsvpc network mode.
+  # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html#network_mode
+  network_mode = "awsvpc"
+
+  # A mapping of tags to assign to the resource.
+  tags = merge(
+    {
+      "Name" = var.service
+    },
+    var.tags,
+  )
+}
+
--- variables.tf
+++ variables.tf
@@ -8,3 +8,50 @@ variable "env" {}
 variable "prefix" {
 }
 
+# TD task definitions
+variable "container_definitions" {
+  type        = string
+  description = "TD A list of valid container definitions provided as a single valid JSON document."
+}
+
+variable "cpu" {
+  default     = "256"
+  type        = string
+  description = "TD The number of cpu units used by the task."
+}
+
+variable "memory" {
+  default     = "512"
+  type        = string
+  description = "TD The amount (in MiB) of memory used by the task."
+}
+
+variable "requires_compatibilities" {
+  default     = ["FARGATE"]
+  type        = list(string)
+  description = "TD A set of launch types required by the task. The valid values are EC2 and FARGATE."
+}
+
+variable "tags" {
+  default     = {}
+  type        = map(string)
+  description = "TD A mapping of tags to assign to all resources."
+}
+
+variable "enabled" {
+  default     = true
+  type        = bool
+  description = "TD Set to false to prevent the module from creating anything."
+}
+
+variable "create_ecs_task_execution_role" {
+  default     = true
+  type        = string
+  description = "Specify true to indicate that ECS Task Execution IAM Role creation."
+}
+
+variable "ecs_task_execution_role_arn" {
+  default     = ""
+  type        = string
+  description = "The ARN of the ECS Task Execution IAM Role."
+}
